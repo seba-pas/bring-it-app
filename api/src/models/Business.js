2d523a53b0c52d958903fa9a0e28cfda4a2946c6
fariasagustin3 <fariasagustin3@gmail.com>
2022-07-19 18:52:02 -0300

    municipios cargados a la base de datos

--- api/src/models/Business.js
+++ api/src/models/Business.js
@@ -4,7 +4,7 @@ const { DataTypes } = require("sequelize");
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define(
-    "Business",
+    "business",
     {
       email: {
         type: DataTypes.STRING,
--- api/src/models/Category.js
+++ api/src/models/Category.js
@@ -4,7 +4,7 @@ const { DataTypes } = require("sequelize");
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define(
-    "Category",
+    "category",
     {
       id: {
         type: DataTypes.INTEGER,
