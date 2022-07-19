ef978c45117dbe54507615cc3041b9854f70ebc2
seba-pas <sebastian.pastorenzi@gmail.com>
2022-07-19 15:45:54 -0300

    feature/homepersonas

--- api/index.js
+++ api/index.js
@@ -20,12 +20,7 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 
-
-
 // // Syncing all the models at once.
-
-
-
 
 conn.sync({ force: false }).then(() => {
   server.listen(process.env.PORT, () => {
