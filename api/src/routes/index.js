5e3eba91503ba84f826528a170ff3927ddadd413
AhrensSG <guillermoahrens@gmail.com>
2022-07-19 20:58:34 -0300

    rutas de pedidos

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,7 +1,26 @@
 const { Router } = require("express");
+const {Purchase, User} = require('../db');
 
 const router = Router();
+const getDbPedidos = async ()=>{
+    return Purchase.findAll({
+        include:{
+            model: User,
+            attributes: ['Email'],
+            through:{
+                atributes: [],
+            },
+        },
+    });
+};
 
+router.get ('/api/pedidos', async (req, res)=>{
+    try {
+        return res.status(200).send(getDbPedidos);
+    } catch (error) {
+        return res.status(404).send(message.error);
+    }
+})
 
 
 module.exports = router;
