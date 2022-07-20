576d9630e8ed6e9b4d8d2779f750f0abb7cf4ff3
seba-pas <sebastian.pastorenzi@gmail.com>
2022-07-20 20:40:18 -0300

    cambios quilombo

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 import image from "../components/img/logoCUT.png";
 import styles from "../styles/NavBar.module.css";
 import SearchBar from "./SearchBar";
-
+//seba
 export default function NavBar() {
   return (
     <div className={styles.navbar}>
