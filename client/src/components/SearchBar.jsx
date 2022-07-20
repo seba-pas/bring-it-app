ec38d219d3cdc73e61abc7af9216efb61df53c77
seba-pas <sebastian.pastorenzi@gmail.com>
2022-07-20 18:13:46 -0300

    navbar/card estilos searchbar

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,27 +1,27 @@
 import React, {useState} from 'react';
-import { useDispatch } from 'react-redux';
-import { getProductsName } from '../actions/index'
+// import { useDispatch } from 'react-redux';
+// import { getProductsName } from '../actions/index'
 
 
 export default function SearchBar({setCurrentPage}) {
-    const dispatch = useDispatch();
-    const [name, setName] = useState('');
+    // const dispatch = useDispatch();
+    // const [name, setName] = useState('');
 
-    function handleInputChange(e){
-        e.preventDefault();
-        setName(e.target.value);
-    }
+    // function handleInputChange(e){
+    //     e.preventDefault();
+    //     setName(e.target.value);
+    // }
 
-    function handleSubmit(e){
-        e.preventDefault();
-        dispatch(getProductsName(name))
-        setCurrentPage(1)
-    }
+    // function handleSubmit(e){
+    //     e.preventDefault();
+    //     dispatch(getProductsName(name))
+    //     setCurrentPage(1)
+    // }
 
   return (
     <div>
-        <input type="text"  placeholder='Buscar' value={name} onChange={(e) => handleInputChange(e)}/>
-        <button type='submit' onClick={(e) => handleSubmit(e)}>Buscar</button>
+        <input type="text"  placeholder='Buscar'  />
+        <button type='submit' >Buscar</button>
     </div>
   )
 }
