e57c1764329a39ca1296f7fc9520ac32a1d7703a
seba-pas <sebastian.pastorenzi@gmail.com>
2022-07-19 21:13:41 -0300

    Commit home

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,8 @@ import "./App.css";
 import React from "react";
 import NavBar from "./components/NavBar";
 // import Home from "./components/Home";
-import HomePersonas from "./components/HomePersonas";
+// import HomePersonas from "./components/HomePersonas";
+
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <NavBar/>
       <h1>BRING ITsssss</h1>
       {/* <Home/> */}
-      <HomePersonas />
+      {/* <HomePersonas /> */}
     </div>
   );
 }
