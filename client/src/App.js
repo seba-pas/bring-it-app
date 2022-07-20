89fa08dc2be57907996da9d4d1c5d0b2e10f0216
Diego Alejandro Pérez García <alejandropgd@outlook.com>
2022-07-20 10:22:45 -0500

    template home business

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import React from "react";
+import HomeBusiness from "./components/HomeBusiness";
+import ProductManager from "./components/ProductManager";
 // import Home from "./components/Home";
 
 function App() {
   return (
     <div>
-      <h1>BRING ITsssss</h1>
-      {/* <Home/> */}
+      {/* <h1>BRING ITsssss</h1> */}
+      <HomeBusiness />
+      {/* <ProductManager /> */}
     </div>
   );
 }
