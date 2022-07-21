ff56c3f5e4f0400dcdf3214928aa7f4e0d2d5101
Agustina Eciolaza <agustinaeciolaza@MacBook-Air-de-Agustina.local>
2022-07-20 21:10:14 -0300

    Creacion de rutas CREAD Travel, Create usuario

--- api/src/models/Product.js
+++ api/src/models/Product.js
@@ -4,9 +4,7 @@ const { DataTypes } = require("sequelize");
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define(
-
     "Product",
-
     {
       id: {
         type: DataTypes.INTEGER,
