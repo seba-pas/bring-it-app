ab0b23c7a05e82473d789f234cca13669f8c96da
Alejandro <tomasgelvez8@gmail.com>
2022-07-21 19:26:00 -0300

    Hice action y reducers

--- client/src/actions/actionsTypes.js
+++ client/src/actions/actionsTypes.js
@@ -1,2 +1,5 @@
 export const GET_ALL_PRODUCTS = 'GET_ALL_PRODUCTS';
 export const GET_PRODUCTS_DETAIL = 'GET_PRODUCTS_DETAIL'
+export const POST_USER = 'POST_USER';
+export const POST_PRODUCT = 'POST_PRODUCT';
+export const PUT_PRODUCT = 'PUT_PRODUCT';
--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,32 +1,78 @@
-import axios from 'axios';
-import { GET_ALL_PRODUCTS, GET_PRODUCTS_DETAIL } from './actionsTypes';
+import axios from "axios";
+import { GET_ALL_PRODUCTS, GET_PRODUCTS_DETAIL,POST_USER, POST_PRODUCT,PUT_PRODUCT} from "./actionsTypes";
 
+
+//Comienzan action PRODUCT
 export const getAllProducts = () => {
-    return async function(dispatch){
-        try {
-            const res = await axios('http://localhost:3001/product');
-            return dispatch({
-                type: GET_ALL_PRODUCTS,
-                action: res.data
-            })
-            
-        } catch (error) {
-          console.log(error)  
-        }
+  return async function (dispatch) {
+    try {
+      const res = await axios.get("http://localhost:3001/api/product");
+      return dispatch({
+        type: GET_ALL_PRODUCTS,
+        action: res.data,
+      });
+    } catch (error) {
+      console.log(error);
     }
-}
+  };
+};
 
 export const getAllProductsDetail = (id) => {
-    return async function(dispatch) {
+  return async function (dispatch) {
+    try {
+      const res = await axios.get(`http://localhost:3001/api/product/${id}`);
+      return dispatch({
+        type: GET_PRODUCTS_DETAIL,
+        action: res.data,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
+
+export const addProduct = (body) => {
+    return async function (dispatch) {
         try {
-            const res = await axios(`http://localhost:3001/product/${id}`)
+          const res = await axios.post(`http://localhost:3001/api/product`, body);
+          return dispatch({
+            type: POST_PRODUCT,
+            action: res.data,
+          });
+        } catch (error) {
+          console.log(error);
+        }
+      };
+}
+
+export const editProduct = ({id,body}) => {
+    return async function (dispatch){
+        try {
+            const res = await axios.update(`http://localhost:3001/api/product/${id}`, body);
             return dispatch({
-                type: GET_PRODUCTS_DETAIL,
+                type:PUT_PRODUCT,
                 action: res.data
             })
-            
         } catch (error) {
-            console.log(error)
+            console.log(error);
         }
     }
 }
+
+//TERMINA ACTION PRODUCT
+
+//COMIENZA ACTION USER
+
+export const addUser = (body) => {
+  return async function (dispatch) {
+    try {
+      const res = await axios.post(`http://localhost:3001/api/user`, body);
+      return dispatch({
+        type: POST_USER,
+        action: res.data,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
