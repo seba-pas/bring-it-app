0f6a3133f8026b98fafac76a02244e2c655ee7af
Alejandro <tomasgelvez8@gmail.com>
2022-07-21 18:39:34 -0300

    Actions listas

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,8 @@ import "./App.css";
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import LandingPage from "./components/LandingPage.jsx";
-import ProductManager from "./components/ProductManager.jsx"
-import HomeBusiness from "./components/HomeBusiness.jsx"
+import ProductManager from "./components/ProductManager.jsx";
+import HomeBusiness from "./components/HomeBusiness.jsx";
 import HomePersonas from "./components/HomePersonas";
 // import NavBar from "./components/NavBar";
 
@@ -14,24 +14,12 @@ function App() {
   return (
     <div className="App">
       <Switch>
-
         <Route exact path="/" component={LandingPage} />
         <Route exact path="/persona" component={HomePersonas} />
         <Route exact path="/empresas" component={HomeBusiness} />
         <Route path="/productmanager/:id" component={ProductManager} />
         <Route exact path="/productmanager" component={ProductManager} />
       </Switch>
-
-      {/* <div>
-      <RegisterUser
-      <NavBar />
-      </div> */}
-
-      {/* <ProductCard /> */}
-
-      {/* <Home/> */}
-      {/* <HomeBusiness /> */}
-      {/* <ProductManager /> */}
     </div>
   );
 }
