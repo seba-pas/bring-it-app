84787fd4fa5e39d32c327dd433de0a5070001d39
Alejandro <tomasgelvez8@gmail.com>
2022-07-21 23:04:54 -0300

    registerUser registerBusiness

--- client/src/components/RegisterBusiness.jsx
+++ client/src/components/RegisterBusiness.jsx
@@ -6,57 +6,50 @@ import style from "../styles/RegisterUser.module.css";
 
 function RegisterBusiness() {
   return (
-    <div className={style.divContainer}>
-      <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
+    <div>
+      <div className={style.divContainer}>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Empresa nombre </Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Ingrese el nombre de la empresa"
+            />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Cuit</Form.Label>
+            <Form.Control type="number" placeholder="Ingrese su nombre" />
+          </Form.Group>
+          <Form.Select className="mt-5" aria-label="Default select example">
+            <option>Selecciona tu pronvincia</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+          </Form.Select>
+          <Form.Group className="mb-3">
+            <Form.Label>Direccion</Form.Label>
+            <Form.Control type="text" placeholder="Ingresa tu direccion" />
+          </Form.Group>
+          {/* <Form.Group className="mb-3">
+          <Form.Label>Logo</Form.Label>
+          <Form.Control type="date" placeholder="Fecha de nacimiento"/>
+        </Form.Group> */}
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Select aria-label="Default select example">
-          <option>Open this select menu</option>
-          <option value="1">One</option>
-          <option value="2">Two</option>
-          <option value="3">Three</option>
-        </Form.Select>
-        <input type="date"/>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
+          <Button variant="primary" className="mt-5" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
     </div>
   );
 }
 
 export default RegisterBusiness;
-{
-  /* <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label">
-          Email address
-        </label>
-        <input
-          type="email"
-          class="form-control"
-          id="exampleFormControlInput1"
-          placeholder="name@example.com"
-        />
-      </div>
-      <div class="mb-3">
-        <label for="exampleFormControlTextarea1" class="form-label">
-          Example textarea
-        </label>
-        <textarea
-          class="form-control"
-          id="exampleFormControlTextarea1"
-          rows="3"
-        ></textarea>
-      </div>
-    </div> */
-}
--- client/src/components/RegisterUser.jsx
+++ client/src/components/RegisterUser.jsx
@@ -11,23 +11,31 @@ function RegisterUser() {
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
         </Form.Group>
-
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
           <Form.Control type="password" placeholder="Password" />
         </Form.Group>
-        <Form.Select aria-label="Default select example">
-          <option>Open this select menu</option>
+        <Form.Group className="mb-3">
+          <Form.Label>Nombre</Form.Label>
+          <Form.Control type="text" placeholder="Ingrese su nombre"/>
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Apellido</Form.Label>
+          <Form.Control type="text" placeholder="Ingrese su nombre"/>
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Fecha nacimiento</Form.Label>
+          <Form.Control type="date" placeholder="Fecha de nacimiento"/>
+        </Form.Group>
+        <Form.Select className="mt-5" aria-label="Default select example">
+          <option>Selecciona tu nacionalidad</option>
           <option value="1">One</option>
           <option value="2">Two</option>
           <option value="3">Three</option>
         </Form.Select>
-        <input type="date" />
-        <Button variant="primary" type="submit">
+       
+        <Button variant="primary" className="mt-5" type="submit">
           Submit
         </Button>
       </Form>
@@ -36,27 +44,3 @@ function RegisterUser() {
 }
 
 export default RegisterUser;
-{
-  /* <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label">
-          Email address
-        </label>
-        <input
-          type="email"
-          class="form-control"
-          id="exampleFormControlInput1"
-          placeholder="name@example.com"
-        />
-      </div>
-      <div class="mb-3">
-        <label for="exampleFormControlTextarea1" class="form-label">
-          Example textarea
-        </label>
-        <textarea
-          class="form-control"
-          id="exampleFormControlTextarea1"
-          rows="3"
-        ></textarea>
-      </div>
-    </div> */
-}
