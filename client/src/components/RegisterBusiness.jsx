29474a65eed291be46c188f9d0b778fbaf5a7b8f
Alejandro <tomasgelvez8@gmail.com>
2022-07-22 01:04:18 -0300

    registers

--- client/src/components/RegisterBusiness.jsx
+++ client/src/components/RegisterBusiness.jsx
@@ -1,14 +1,24 @@
-import React from "react";
+import { React, useState } from "react";
+import { useDispatch } from "react-redux";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+// import { useHistory } from "react-router-dom";
+// import { addUser } from "../actions/index.js";
+
 // import style from "../styles/RegisterBusiness.module.css";
 
 function RegisterBusiness() {
+  
+
   return (
     <div>
-      <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
+      <Form onSubmit={(e) => handleSubmit(e)}>
+        <Form.Group
+          className="mb-3"
+          controlId="formBasicEmail"
+          onSubmit={(e) => handleSubmit(e)}
+        >
           <Form.Label>Email address</Form.Label>
           <Form.Control
             type="email"
@@ -17,6 +27,7 @@ function RegisterBusiness() {
             id="email"
             required
             placeholder="Enter email"
+            onChange={(e) => handleChange(e)}
           />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPassword">
@@ -24,6 +35,7 @@ function RegisterBusiness() {
           <Form.Control
             type="password"
             placeholder="Password"
+            onChange={(e) => handleChange(e)}
             value={input.password}
             name="password"
             id="password"
@@ -35,6 +47,7 @@ function RegisterBusiness() {
           <Form.Control
             type="text"
             placeholder="Ingrese el nombre de la empresa"
+            onChange={(e) => handleChange(e)}
             value={input.empresaNombre}
             name="empresaNombre"
             id="empresaNombre"
@@ -50,6 +63,7 @@ function RegisterBusiness() {
             id="cuit"
             required
             placeholder="Ingrese su nombre"
+            onChange={(e) => handleChange(e)}
           />
         </Form.Group>
         <Form.Select className="mt-5" aria-label="Default select example">
@@ -67,6 +81,7 @@ function RegisterBusiness() {
             id="direccion"
             required
             placeholder="Ingresa tu direccion"
+            onChange={(e) => handleChange(e)}
           />
         </Form.Group>
 
--- client/src/components/RegisterUser.jsx
+++ client/src/components/RegisterUser.jsx
@@ -1,32 +1,120 @@
-import React from "react";
+import { useHistory } from "react-router-dom";
+import { addUser } from "../actions/index.js";
+import { React, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { useDispatch } from "react-redux";
+
 import style from "../styles/RegisterUser.module.css";
 
 function RegisterUser() {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const [errors, setErrors] = useState({});
+  const [input, setInput] = useState({
+    email: "",
+    password: "",
+    nombre: "",
+    apellido: "",
+    fechaNacimiento: "",
+    nacionalidad: [],
+  });
+
+  function handleChange(e) {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (
+      input.email !== "" &&
+      input.password !== "" &&
+      input.nombre !== "" &&
+      input.apellido !== "" &&
+      input.fechaNacimiento !== "" &&
+      input.nacionalidad.length !== 0
+    ) {
+      dispatch(console.log(addUser(input)));
+      alert("El usuario fue creada con exito!");
+      setInput({
+        email: "",
+        password: "",
+        nombre: "",
+        apellido: "",
+        fechaNacimiento: "",
+        nacionalidad: [],
+      });
+      history.push("/persona");
+    } else {
+      alert("¡Faltan los elementos necesarios!");
+    }
+  }
   return (
     <div className={style.divContainer}>
-      <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
+      <Form onSubmit={(e) => handleSubmit(e)}>
+        <Form.Group className="mb-3">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            placeholder="Enter email"
+            type="email"
+            value={input.email}
+            name="email"
+            id="email"
+            required
+            onChange={(e) => handleChange(e)}
+          />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Group className="mb-3">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            onChange={(e) => handleChange(e)}
+            value={input.password}
+            name="password"
+            id="password"
+            required
+          />
         </Form.Group>
         <Form.Group className="mb-3">
           <Form.Label>Nombre</Form.Label>
-          <Form.Control type="text" placeholder="Ingrese su nombre"/>
+          <Form.Control
+            type="text"
+            placeholder="Ingrese su nombre"
+            onChange={(e) => handleChange(e)}
+            value={input.nombre}
+            name="nombre"
+            id="nombre"
+            required
+          />
         </Form.Group>
         <Form.Group className="mb-3">
           <Form.Label>Apellido</Form.Label>
-          <Form.Control type="text" placeholder="Ingrese su nombre"/>
+          <Form.Control
+            type="text"
+            placeholder="Ingrese su apellido"
+            onChange={(e) => handleChange(e)}
+            value={input.apellido}
+            name="apellido"
+            id="apellido"
+            required
+          />
         </Form.Group>
         <Form.Group className="mb-3">
           <Form.Label>Fecha nacimiento</Form.Label>
-          <Form.Control type="date" placeholder="Fecha de nacimiento"/>
+          <Form.Control
+            type="date"
+            placeholder="Fecha de nacimiento"
+            onChange={(e) => handleChange(e)}
+            value={input.fechaNacimiento}
+            name="fechaNacimiento"
+            id="fechaNacimiento"
+            required
+          />
         </Form.Group>
         <Form.Select className="mt-5" aria-label="Default select example">
           <option>Selecciona tu nacionalidad</option>
@@ -34,7 +122,7 @@ function RegisterUser() {
           <option value="2">Two</option>
           <option value="3">Three</option>
         </Form.Select>
-       
+
         <Button variant="primary" className="mt-5" type="submit">
           Submit
         </Button>
