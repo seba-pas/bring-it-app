9c3b36062fa7cb292cd6ef890e98cc8366ce20b3
seba-pas <sebastian.pastorenzi@gmail.com>
2022-07-23 04:55:01 -0300

    Front/Fix

--- client/src/reducer/index.js
+++ client/src/reducer/index.js
@@ -70,51 +70,52 @@ export default function rootReducer(state = initialState, action) {
           ...state,
           products: action.payload,
         };
-      }       
-      };
-    case 'ORDER_BY_PRICE':
-      let sortedPrice = action.payload === 'asc' ?
-            state.allProducts.sort(function (a, b) {
-                if(a.price > b.price) {
-                    return 1;
-                }
-                if(b.price > a.price) {
-                    return -1;
-                }
-                return 0;
-            }) :
-            state.allProducts.sort(function (a, b) {
-                if(a.price > b.price) {
-                    return -1;
-                }
-                if(b.price > a.price) {
-                    return 1;
-                }
-                return 0;
-            })
-            return {
-                ...state,
-                products: sortedPrice
-            };
-    case 'GET_CATEGORIES':
-            return {
-                ...state,
-                categories: action.payload
-            };
-    case 'FILTER_BY_CATEGORY':
-        const allProducts = state.allProducts
-        const filterCategory = action.payload === 'All' ? allProducts :
-              allProducts.filter(e => e.categories[0].name === action.payload)
-              console.log(filterCategory)
-              console.log(action.payload, 'Payload')
-              return{
-                  ...state,
-                  products: filterCategory
-            };
+      }
 
+    case "ORDER_BY_PRICE":
+      let sortedPrice =
+        action.payload === "asc"
+          ? state.allProducts.sort(function (a, b) {
+              if (a.price > b.price) {
+                return 1;
+              }
+              if (b.price > a.price) {
+                return -1;
+              }
+              return 0;
+            })
+          : state.allProducts.sort(function (a, b) {
+              if (a.price > b.price) {
+                return -1;
+              }
+              if (b.price > a.price) {
+                return 1;
+              }
+              return 0;
+            });
+      return {
+        ...state,
+        products: sortedPrice,
+      };
+    case "GET_CATEGORIES":
+      return {
+        ...state,
+        categories: action.payload,
+      };
+    case "FILTER_BY_CATEGORY":
+      const allProducts = state.allProducts;
+      const filterCategory =
+        action.payload === "All"
+          ? allProducts
+          : allProducts.filter((e) => e.categories[0].name === action.payload);
+      console.log(filterCategory);
+      console.log(action.payload, "Payload");
+      return {
+        ...state,
+        products: filterCategory,
+      };
 
     case "SET_PRODUCT_DETAIL":
-
       return {
         ...state,
         productsDetail: {},
