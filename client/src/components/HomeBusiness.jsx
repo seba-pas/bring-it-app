db885d7f6825db1d8763dd9c1ee50e710c6faaff
FelipeAviani <felipeaviani@gmail.com>
2022-07-27 00:30:07 -0300

    Arreglado Business

--- client/src/components/HomeBusiness.jsx
+++ client/src/components/HomeBusiness.jsx
@@ -8,159 +8,162 @@ import { getAllBusiness, getAllProducts, getCategories } from '../actions';
 import NavBarBusiness from "./NavBarBusiness"
 
 function HomeBusiness() {
-  const gState = useSelector((state) => state);
-  const dispatch = useDispatch();
-  const history = useHistory();
 
-  useEffect(() => {
-    dispatch(getAllProducts());
-  }, [dispatch, gState.deleteProduct]);
-  useEffect(() => {
-    dispatch(getAllBusiness());
-  }, [dispatch]);
+    const gState = useSelector((state) => state);
+    const dispatch = useDispatch();
+    const history = useHistory();
 
-  const [input, setInput] = useState({
-    perfil: "",
-    allProducts: [],
-  });
 
-  useEffect(() => {
-    setInput((prevInput) => {
-      return {
-        ...prevInput,
-        allProducts: gState.allProducts,
-      };
+    useEffect(() => {
+        dispatch(getAllProducts());
+    }, [dispatch, gState.deleteProduct]);
+
+    useEffect(() => {
+        dispatch(getAllBusiness());
+        dispatch(getCategories());
+    }, [dispatch]);
+
+    const [input, setInput] = useState({
+        perfil: "",
+        products: [],
+        businessInfo: {},
     });
-  }, [gState.allProducts]);
 
-  //     if (input.perfil === "email") history.push("/perfil");  //   console.log("click en", input.perfil, " ", input.businessEmailState)
-  //     else if (input.perfil === "close") history.push("/");  //   console.log("click en", input.perfil, " ", input.businessEmailState)
-  // }, [input.perfil]);
+    useEffect(() => {
+        setInput((prevInput) => {
+            return {
+                ...prevInput,
+                businessInfo: { ...gState.businessEditInfo },
+                products: gState.allProducts.filter(e => e.businessEmail === gState.businessEditInfo.email),
+            }
+        })
+    }, [gState]);
 
-  useEffect(() => {
-    setInput((prevInput) => {
-      return {
-        ...prevInput,
-        businessEmailState: gState.businessEmail,
-      };
-    });
-  }, [gState.businessEmail]);
+    useEffect(() => {
+        setInput((prevInput) => {
+            return {
+                ...prevInput,
+                products: gState.allProducts.filter(e => e.businessEmail === gState.businessEditInfo.email),
+            }
+        })
 
-  const handleOnChange = (event) => {
-    event.preventDefault();
-    setInput((prevInput) => {
-      return {
-        ...prevInput,
+    }, [gState.allProducts]);
 
-        [event.target.name]: event.target.value,
-      };
-    });
-  };
+    useEffect(() => {
+        if (input.perfil === "email") history.push("/perfil");
+        else if (input.perfil === "close") history.push("/");
+    }, [input.perfil]);
 
-  let products = input.allProducts.filter(
-    (e) => e.businessEmail === input.businessEmailState
-  );
 
-  let soldProducts = [
-    {
-      id: 1,
-      productImage:
-        "https://thumbs.dreamstime.com/b/imagen-del-icono-de-la-u%C3%B1a-del-pulgar-de-la-fotograf%C3%ADa-o-del-bot%C3%B3n-de-la-galer%C3%ADa-de-la-imagen-84717969.jpg",
-      productName: "zapatos>",
-      amount: 2,
-      description: "size:41",
-      clientName: "Agustina Eciolaza",
-      clientPhone: 555 - 5555 - 5555,
-      date: "19-07-22",
-      BringerPending: true,
-      BringerName: "",
-      BringerContact: null,
-    },
-    {
-      id: 2,
-      productImage:
-        "https://thumbs.dreamstime.com/b/imagen-del-icono-de-la-u%C3%B1a-del-pulgar-de-la-fotograf%C3%ADa-o-del-bot%C3%B3n-de-la-galer%C3%ADa-de-la-imagen-84717969.jpg",
-      productName: "bag",
-      amount: 1,
-      description: "color:brown",
-      clientName: "Cele Chelu",
-      clientPhone: 222 - 2222 - 2222,
-      date: "18-07-22",
-      BringerPending: false,
-      BringerName: "Sebastian",
-      BringerContact: 333 - 33333 - 333,
-    },
-    {
-      id: 3,
-      productImage:
-        "https://thumbs.dreamstime.com/b/imagen-del-icono-de-la-u%C3%B1a-del-pulgar-de-la-fotograf%C3%ADa-o-del-bot%C3%B3n-de-la-galer%C3%ADa-de-la-imagen-84717969.jpg",
-      productName: "vino tinto",
-      amount: 10,
-      description: "asd",
-      clientName: "Felipe Aviani",
-      clientPhone: 777 - 5555 - 5555,
-      date: "07-07-22",
-      BringerPending: true,
-      BringerName: "",
-      BringerContact: null,
-    },
-    {
-      id: 4,
-      productImage:
-        "https://thumbs.dreamstime.com/b/imagen-del-icono-de-la-u%C3%B1a-del-pulgar-de-la-fotograf%C3%ADa-o-del-bot%C3%B3n-de-la-galer%C3%ADa-de-la-imagen-84717969.jpg",
-      productName: "vino tinto",
-      amount: 10,
-      description: "asd",
-      clientName: "Guillermo Ahrens",
-      clientPhone: 777 - 5555 - 5555,
-      date: "07-07-22",
-      BringerPending: false,
-      BringerName: "Agustín Farias",
-      BringerContact: 3213 - 5445 - 55,
-    },
-    {
-      id: 5,
-      productImage:
-        "https://thumbs.dreamstime.com/b/imagen-del-icono-de-la-u%C3%B1a-del-pulgar-de-la-fotograf%C3%ADa-o-del-bot%C3%B3n-de-la-galer%C3%ADa-de-la-imagen-84717969.jpg",
-      productName: "reloj",
-      amount: 10,
-      description: "asd",
-      clientName: "Tomas Gelvez",
-      clientPhone: 777 - 5555 - 5555,
-      date: "07-07-22",
-      BringerPending: true,
-      BringerName: "",
-      BringerContact: null,
-    },
-    {
-      id: 6,
-      productImage:
-        "https://thumbs.dreamstime.com/b/imagen-del-icono-de-la-u%C3%B1a-del-pulgar-de-la-fotograf%C3%ADa-o-del-bot%C3%B3n-de-la-galer%C3%ADa-de-la-imagen-84717969.jpg",
-      productName: "gafas",
-      amount: 1,
-      description: "asd",
-      clientName: "Sebastina",
-      clientPhone: 777 - 5555 - 5555,
-      date: "07-07-22",
-      BringerPending: false,
-      BringerName: "Cele ",
-      BringerContact: 3213 - 5445 - 55,
-    },
-    {
-      id: 7,
-      productImage:
-        "https://thumbs.dreamstime.com/b/imagen-del-icono-de-la-u%C3%B1a-del-pulgar-de-la-fotograf%C3%ADa-o-del-bot%C3%B3n-de-la-galer%C3%ADa-de-la-imagen-84717969.jpg",
-      productName: "gafas",
-      amount: 1,
-      description: "asd",
-      clientName: "Agustín Farias",
-      clientPhone: 777 - 5555 - 5555,
-      date: "07-07-22",
-      BringerPending: false,
-      BringerName: "Agustina",
-      BringerContact: 3213 - 5445 - 55,
-    },
-  ];
+    const handleOnChange = (event) => {
+        event.preventDefault();
+        setInput((prevInput) => {
+            return {
+                ...prevInput,
+                [event.target.name]: event.target.value,
+            }
+        });
+    }
+
+
+
+
+
+    let soldProducts = [
+        {
+            id: 1,
+            productImage: "https://thumbs.dreamstime.com/b/imagen-del-icono-de-la-u%C3%B1a-del-pulgar-de-la-fotograf%C3%ADa-o-del-bot%C3%B3n-de-la-galer%C3%ADa-de-la-imagen-84717969.jpg",
+            productName: "zapatos>",
+            amount: 2,
+            description: "size:41",
+            clientName: "Agustina Eciolaza",
+            clientPhone: 555 - 5555 - 5555,
+            date: "19-07-22",
+            BringerPending: true,
+            BringerName: "",
+            BringerContact: null,
+        },
+        {
+            id: 2,
+            productImage: "https://thumbs.dreamstime.com/b/imagen-del-icono-de-la-u%C3%B1a-del-pulgar-de-la-fotograf%C3%ADa-o-del-bot%C3%B3n-de-la-galer%C3%ADa-de-la-imagen-84717969.jpg",
+            productName: "bag",
+            amount: 1,
+            description: "color:brown",
+            clientName: "Cele Chelu",
+            clientPhone: 222 - 2222 - 2222,
+            date: "18-07-22",
+            BringerPending: false,
+            BringerName: "Sebastian",
+            BringerContact: 333 - 33333 - 333,
+        },
+        {
+            id: 3,
+            productImage: "https://thumbs.dreamstime.com/b/imagen-del-icono-de-la-u%C3%B1a-del-pulgar-de-la-fotograf%C3%ADa-o-del-bot%C3%B3n-de-la-galer%C3%ADa-de-la-imagen-84717969.jpg",
+            productName: "vino tinto",
+            amount: 10,
+            description: "asd",
+            clientName: "Felipe Aviani",
+            clientPhone: 777 - 5555 - 5555,
+            date: "07-07-22",
+            BringerPending: true,
+            BringerName: "",
+            BringerContact: null,
+        },
+        {
+            id: 4,
+            productImage: "https://thumbs.dreamstime.com/b/imagen-del-icono-de-la-u%C3%B1a-del-pulgar-de-la-fotograf%C3%ADa-o-del-bot%C3%B3n-de-la-galer%C3%ADa-de-la-imagen-84717969.jpg",
+            productName: "vino tinto",
+            amount: 10,
+            description: "asd",
+            clientName: "Guillermo Ahrens",
+            clientPhone: 777 - 5555 - 5555,
+            date: "07-07-22",
+            BringerPending: false,
+            BringerName: "Agustín Farias",
+            BringerContact: 3213 - 5445 - 55,
+        },
+        {
+            id: 5,
+            productImage: "https://thumbs.dreamstime.com/b/imagen-del-icono-de-la-u%C3%B1a-del-pulgar-de-la-fotograf%C3%ADa-o-del-bot%C3%B3n-de-la-galer%C3%ADa-de-la-imagen-84717969.jpg",
+            productName: "reloj",
+            amount: 10,
+            description: "asd",
+            clientName: "Tomas Gelvez",
+            clientPhone: 777 - 5555 - 5555,
+            date: "07-07-22",
+            BringerPending: true,
+            BringerName: "",
+            BringerContact: null,
+        },
+        {
+            id: 6,
+            productImage: "https://thumbs.dreamstime.com/b/imagen-del-icono-de-la-u%C3%B1a-del-pulgar-de-la-fotograf%C3%ADa-o-del-bot%C3%B3n-de-la-galer%C3%ADa-de-la-imagen-84717969.jpg",
+            productName: "gafas",
+            amount: 1,
+            description: "asd",
+            clientName: "Sebastina",
+            clientPhone: 777 - 5555 - 5555,
+            date: "07-07-22",
+            BringerPending: false,
+            BringerName: "Cele ",
+            BringerContact: 3213 - 5445 - 55,
+        },
+        {
+            id: 7,
+            productImage: "https://thumbs.dreamstime.com/b/imagen-del-icono-de-la-u%C3%B1a-del-pulgar-de-la-fotograf%C3%ADa-o-del-bot%C3%B3n-de-la-galer%C3%ADa-de-la-imagen-84717969.jpg",
+            productName: "gafas",
+            amount: 1,
+            description: "asd",
+            clientName: "Agustín Farias",
+            clientPhone: 777 - 5555 - 5555,
+            date: "07-07-22",
+            BringerPending: false,
+            BringerName: "Agustina",
+            BringerContact: 3213 - 5445 - 55,
+        },
+    ]
+
+
 
     return (
         <div>
