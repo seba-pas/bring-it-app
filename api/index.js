ba5636f20f9bfc7cbb8091e3c98bd87366514982
Sebastian Pastorenzi <102265528+seba-pas@users.noreply.github.com>
2022-07-30 17:59:50 -0300

    push

--- api/index.js
+++ api/index.js
@@ -30,7 +30,7 @@ const { conn } = require("./src/db.js");
 
 // // Syncing all the models at once.
 
-conn.sync({ force: false}).then(() => {
+conn.sync({ force: true}).then(() => {
   server.listen(process.env.PORT || 3001, async () => {
    
     console.log("BRING IT ON! Listening..."); // eslint-disable-line no-console
