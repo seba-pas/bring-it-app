f0bea383c7574ee37adf538c7eb9fe72cdaf9425
Diego Alejandro Pérez García <alejandropgd@outlook.com>
2022-08-08 16:56:33 -0500

    pb

--- client/src/components/HomeUserPurchase.jsx
+++ client/src/components/HomeUserPurchase.jsx
@@ -100,6 +100,7 @@ function HomeUserPurchase() {
   const handleShow = (id) => {
     setShow((showId) => (showId === id ? null : id));
   };
+
   const searchMatch = (idPurchase) => {
     dispatch(getMatch(idPurchase));
   };
