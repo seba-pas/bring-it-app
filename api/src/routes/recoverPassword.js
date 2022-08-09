26b9412ef17450318d6a8360f2762f750e8d7078
fariasagustin3 <fariasagustin3@gmail.com>
2022-08-09 13:58:38 -0300

    cambiar despues el package.json del client

--- api/src/routes/recoverPassword.js
+++ api/src/routes/recoverPassword.js
@@ -8,23 +8,23 @@ router.post("/", async(req, res) => {
 	console.log("1", email)
 	try {
 			// nodemailer
-			let transporter = nodemailer.createTransport({
-        	host: 'smtp.gmail.com',
-        	port: 465,
-        	secure: true,
-        	auth: {
-          	user: 'bringit662@gmail.com',
-          	pass: 'baiepxymtdopmjuj'
-       		}
-      	});
+			// let transporter = nodemailer.createTransport({
+   //      	host: 'smtp.gmail.com',
+   //      	port: 465,
+   //      	secure: true,
+   //      	auth: {
+   //        	user: 'bringit662@gmail.com',
+   //        	pass: 'baiepxymtdopmjuj'
+   //     		}
+   //    	});
 
-      	await transporter.sendMail({
-        	from: "Bring It App <bringit662@gmail.com>",
-        	to: email,
-        	subject: "Cambio de contraseña",
-        	html: `<h3>Solicitaste un cambio de contraseña, podrás hacerlo haciendo click en este <p><a href="http://localhost:3000/recuperarContrasenia">enlace</a></p></h3>`
-      	})
-      	console.log("2", email)
+   //    	await transporter.sendMail({
+   //      	from: "Bring It App <bringit662@gmail.com>",
+   //      	to: email,
+   //      	subject: "Cambio de contraseña",
+   //      	html: `<h3>Solicitaste un cambio de contraseña, podrás hacerlo haciendo click en este <p><a href="http://localhost:3000/recuperarContrasenia">enlace</a></p></h3>`
+   //    	})
+   //    	console.log("2", email)
 
 
       res.status(200).send(email)
