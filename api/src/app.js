971969b11df4897c9b1a7c27a69dd00b4647f0d4
tomasgelvez <83987176+tomasgelvez@users.noreply.github.com>
2022-08-10 00:15:15 -0300

    Update app.js

--- api/src/app.js
+++ api/src/app.js
@@ -37,9 +37,8 @@ server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
-server.use((req, res, next) => { //Alej
-  // res.header("Access-Control-Allow-Origin", "https://bring-it-app.vercel.app"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+server.use((req, res, next) => { //aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+  res.header("Access-Control-Allow-Origin", "https://bring-it-app.vercel.app"); // update to match the domain you will make the request from
 
 
 
